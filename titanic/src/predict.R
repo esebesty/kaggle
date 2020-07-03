f4e856e9a1c40acb1133d6bd8de9f650950934fa
Endre Sebestyen <endre.sebestyen@gmail.com>
2020-07-03 19:12:27 +0200

    Started correcting missing values

--- titanic/src/predict.R
+++ titanic/src/predict.R
@@ -1,10 +1,35 @@
 library("tidyverse")
+library("viridis")
 
 train <- read_csv("titanic/data/csv/train.csv", col_types = "iiiccdiicdcc")
 test  <- read_csv("titanic/data/csv/test.csv",  col_types = "iiccdiicdcc")
 
-# TODO
-# deal with NA values
+full <- train
+
+# missing values ----------------------------------------------------------
+
+train_na <- train %>%
+    gather(variable, value, -PassengerId) %>%
+    mutate(is_na = ifelse(is.na(value), "Yes", "No"))
+
+ggplot(train_na, aes(x = PassengerId, y = variable, fill = is_na)) +
+    geom_tile() +
+    scale_fill_viridis(discrete = TRUE) +
+    theme_minimal()
+
+# missing embarked value --------------------------------------------------
+
+train_embarked <- train %>%
+    filter(!is.na(Embarked))
+
+ggplot(train_embarked, aes(x = Embarked, y = Fare, fill = factor(Pclass))) +
+    geom_boxplot() +
+    scale_fill_viridis(discrete = TRUE) +
+    theme_minimal()
+
+full$Embarked[is.na(full$Embarked)] <- 'C'
+
+# logistic regression -----------------------------------------------------
 
 titanic_logistic <- glm(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked,
                         data = train, family = binomial)
@@ -17,5 +42,3 @@ test_full <- cbind(test, titanic_pred) %>%
     rename(Survived = titanic_pred)
 
 write_csv(test_full, "titanic/res/submission01.csv")
-
-
